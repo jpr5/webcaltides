8699736f334bf4b4d7c61b8ffb4118c7b809b5a6
Jordan Ritter <jpr5@darkridge.com>
2024-01-31 14:54:28 -0800

    Re-org puma config

--- puma.rb
+++ puma.rb
@@ -1,7 +1,12 @@
-root = File.expand_path(".")
-  
-bind  "unix://" + root + "/webcaltides.sock"
 workers 0
-threads 4, 10
+threads 4, 10 # high max because agents tend to sync on time, which can overwhelm us
 
-stdout_redirect "/srv/http/logs/webcaltides.log", "/srv/http/logs/webcaltides.log", true
+log_requests false # controlled from sinatra, don't want dupe
+
+env = ENV["FU_ENV"] || ENV["RAILS_ENV"] || ENV["RACK_ENV"]
+
+if env == "production"
+    root = File.expand_path(".")
+    bind "unix://" + root + "/webcaltides.sock"
+    stdout_redirect "/srv/http/logs/webcaltides.log", "/srv/http/logs/webcaltides.log", true
+end
