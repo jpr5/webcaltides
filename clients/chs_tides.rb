1107d59e9c769af592c2f94b3a02061388806513
Jordan Ritter <jpr5@darkridge.com>
2024-02-07 00:02:43 -0800

    Add catch for CHS stations returning empty datasets

--- clients/chs_tides.rb
+++ clients/chs_tides.rb
@@ -52,6 +52,31 @@ module Clients
 
             logger.debug "parsing tide predictions for #{station.id} from API #{API_URL}"
             data = JSON.parse(json) rescue []
+
+            # So this happened: station 5cebf1e23d0f4a073c4bbfb4 returned empty data.  It is type:
+            # DISCONTINUED, operating: false, which would strongly imply we should filter those out
+            # from the list, however a different station 5cebf1df3d0f4a073c4bbcb9 also has the same
+            # type/operating but *DOES* return tide data.
+            #
+            # Not sure what to do ... obviously the prev_value calc errors out if there's no data,
+            # so it seems dumb to just let that happen if we know to maybe expect this condition and
+            # could handle it.
+            #
+            # We could filter out stations that are not operating/discontinued, but clearly there's
+            # at least one example where data is still being returned so it feels like we could end
+            # up cutting the station list short.
+            # TODO: need to do more research to see how many stations in the list are non-operating
+            # but returning tide data.
+            #
+            # We can return nil but that will result in a 404 to the caller, despite the station
+            # showing up in the search list.  Until I have time for the TODO above, this is the
+            # least shitty option..
+
+            if data.length == 0
+                logger.error "!! got empty tide data for station #{station.id}"
+                return nil
+            end
+
             prev_value = data[1]['value']
 
             return data.map do |jt|
