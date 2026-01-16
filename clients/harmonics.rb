f77928bd9388d4ba6a5fa127ae3167611b259fc0
Jordan Ritter <jpr5@darkridge.com>
2026-01-16 10:07:25 -0800

    Extend Harmonics client to predict currents

--- clients/harmonics.rb
+++ clients/harmonics.rb
@@ -7,6 +7,8 @@ module Clients
     class Harmonics < Base
         include TimeWindow
 
+        attr_reader :engine
+
         # Harmonics usually cover a full year or more, but we'll stick to the window pattern
         self.window_size = 13.months
 
@@ -16,16 +18,40 @@ module Clients
         end
 
         def tide_stations
-            return [] unless File.exist?(@engine.harmonics_file)
+            return [] unless File.exist?(@engine.harmonics_file) || File.exist?(@engine.ticon_file)
 
-            @engine.stations.map { |s| Models::Station.from_hash(s.stringify_keys) }
+            @engine.stations.select { |s| s['type'] == 'tide' }.map { |s| Models::Station.from_hash(s.stringify_keys) }
+        end
+
+        def current_stations
+            return [] unless File.exist?(@engine.harmonics_file) || File.exist?(@engine.ticon_file)
+
+            @engine.stations.select { |s| s['type'] == 'current' }.map { |s| Models::Station.from_hash(s.stringify_keys) }
+        end
+
+        def current_data_for(station, around)
+            # For now, XTide/TICON current data is predicted the same way as tide data
+            # but represents velocity. generate_predictions returns height/velocity.
+            tide_data_for(station, around).map do |d|
+                Models::CurrentData.new(
+                    type: d.type.downcase == 'high' ? 'flood' : 'ebb', # Mapping peak types to flood/ebb for now
+                    time: d.time,
+                    prediction: d.prediction,
+                    velocity_major: d.prediction,
+                    units: d.units,
+                    depth: station.depth,
+                    url: d.url
+                )
+            end
         end
 
         def tide_data_for(station, around)
             start_time = beginning_of_window(around)
             end_time = end_of_window(around)
 
-            predictions = @engine.generate_predictions(station.id, start_time, end_time)
+            # Use bid if available (e.g. for currents with different depths), fallback to id
+            lookup_id = station.bid || station.id
+            predictions = @engine.generate_predictions(lookup_id, start_time, end_time)
             peaks = @engine.detect_peaks(predictions)
 
             peaks.map do |p|
