9342f93c32c397e9094d3d553d7643d80761cbc2
Jordan Ritter <jpr5@darkridge.com>
2026-01-23 15:27:02 -0800

    Revert railway settings to avert NOAA/CHS throttling

--- puma.rb
+++ puma.rb
@@ -1,16 +1,20 @@
 env = ENV["FU_ENV"] || ENV["RAILS_ENV"] || ENV["RACK_ENV"]
 log_requests false # controlled from sinatra, don't want dupe
 
+# LEARNING: we could significantly parallelize to increase throughpu under puma, but
+# at the end of the day, we're limited by calls to NOAA and CHS, who will throttle
+# us if we send them too many concurrent requests.  So it does us no good to scale
+# very far, because it'll just stall our users' requests as they retry.  IOW we'll
+# get more net throughput not triggering the throttle.
+
+workers 0
+threads 4, 10
+
 if env == "production" || env == "staging"
     if ENV["RAILWAY_ENVIRONMENT"]
         # Take advantage of Railway's available resources
-        workers 8
-        threads 4, 10
-        port = ENV.fetch("PORT", 3000)
+        port ENV.fetch("PORT", 3000)
     else
-        workers 0
-        threads 4, 20 # high max because agents tend to sync on time, which can overwhelm us
-
         root = File.expand_path(".")
         bind "unix://" + root + "/webcaltides.sock"
         stdout_redirect("/srv/webcaltides/logs/webcaltides.log", "/srv/webcaltides/logs/webcaltides.log", true)
