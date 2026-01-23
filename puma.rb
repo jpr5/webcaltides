0d65cebdef6e83851de292a21803f393aee88e0f
Jordan Ritter <jpr5@darkridge.com>
2026-01-23 14:29:58 -0800

    Add railway-specific settings

--- puma.rb
+++ puma.rb
@@ -1,14 +1,16 @@
-workers 0
-threads 4, 20 # high max because agents tend to sync on time, which can overwhelm us
-
+env = ENV["FU_ENV"] || ENV["RAILS_ENV"] || ENV["RACK_ENV"]
 log_requests false # controlled from sinatra, don't want dupe
 
-env = ENV["FU_ENV"] || ENV["RAILS_ENV"] || ENV["RACK_ENV"]
-
-if env == "production"
+if env == "production" || env == "staging"
     if ENV["RAILWAY_ENVIRONMENT"]
-        port ENV.fetch("PORT", 3000)
+        # Take advantage of Railway's available resources
+        workers 8
+        threads 4, 10
+        port = ENV.fetch("PORT", 3000)
     else
+        workers 0
+        threads 4, 20 # high max because agents tend to sync on time, which can overwhelm us
+
         root = File.expand_path(".")
         bind "unix://" + root + "/webcaltides.sock"
         stdout_redirect("/srv/webcaltides/logs/webcaltides.log", "/srv/webcaltides/logs/webcaltides.log", true)
